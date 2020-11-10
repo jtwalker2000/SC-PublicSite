f27ee3e9b5731e8b5bacdec0f56b87e758642673
thedocbwarren <jedi.master@augmentedjs.com>
2020-11-09 20:54:17 -0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import "material-icons";
 import "presentation-css";
 import "./styles/main.scss";
 
-import("presentation-elements");
 import { Elements } from "@augmentedjs/elements";
 /* router */
 import router from "./router.jsx";
@@ -12,11 +11,6 @@ import Main from "./views/main.jsx";
 
 Elements.render(<Main id="article" name="main" className="article"/>, document.getElementById("app"));
 
-// interesting performance tunning
-// window.addEventListener("DOMContentLoaded", (event) => {
-//   console.debug("DOM fully loaded and parsed, loading icons");
-//   import("material-icons");
-// });
 window.addEventListener("load", (event) => {
   console.debug("DOM fully loaded and parsed, and painted, swaping fonts");
   /* fonts */
